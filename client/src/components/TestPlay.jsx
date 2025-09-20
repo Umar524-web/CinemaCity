20aa653f52c1a074179ae94580aed56d96500dfd
umar524 <ufaruque524@gmail.com>
2025-09-20 22:57:06 +0530

    first commit

---
+++ client/src/components/TestPlay.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+const TestPlay = () => {
+  return (
+    <div className='ps-6 md:px-16 lg:px-16 xl:px-44 py-20'>
+        <p>Trail</p>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/u3V5KDHRQvk?si=VFRJ0Jqxz_YuOYbY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+  )
+}
+
+export default TestPlay       
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import TrailersSection from '../components/TrailersSection'
+import TestPlay from '../components/TestPlay'
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <FeaturedSection />
+      <TrailersSection />
+      <TestPlay />
+        
+    </>
+  )
+}
+
+export default Home
