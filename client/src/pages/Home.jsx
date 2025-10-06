e49ddfc701ce919c2c1aac50e35c5a37825249c3
umar524 <ufaruque524@gmail.com>
2025-10-07 01:15:47 +0530

    project completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
 import TrailersSection from '../components/TrailersSection'
-import TestPlay from '../components/TestPlay'
+// import TestPlay from '../components/TestPlay'
 
 const Home = () => {
   return (
@@ -10,7 +10,7 @@ const Home = () => {
       <HeroSection />
       <FeaturedSection />
       <TrailersSection />
-      <TestPlay />
+      {/* <TestPlay /> */}
         
     </>
   )
